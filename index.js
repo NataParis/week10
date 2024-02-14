3e3849653924c956533758b56a0a7d0f1896fcb6
NataParis <virtx@mail.ru>
2024-02-14 15:55:48 +0100

    'week10'

---
+++ index.js
@@ -0,0 +1,14 @@
+const img = document.getElementById("pic");
+
+
+function getPrevPic() {
+    img.src="./assets/pink.jpg";
+}
+function getNextPic() {
+    img.src="./assets/azur.jpg";
+}
+
+function mySlogan() {
+   console.log("Я учу JavaScript!");
+};
+mySlogan()
